b8d320a078855df099534589a2a7c4d9500c73ed
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-06-04 17:56:39 +0530

    login screen completed

--- trip_plan/lib/src/screens/login/login_screen.dart
+++ trip_plan/lib/src/screens/login/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:lottie/lottie.dart';
+import 'package:trip_plan/src/screens/Sign-up/sign_up_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,46 +10,92 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
-        titleTextStyle: TextStyle(
+        titleTextStyle: const TextStyle(
             fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
         title: const Text('Login to Trip World'),
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20),
           child: Column(
             children: [
               Lottie.asset('assets/animation/login.json',
                   height: MediaQuery.of(context).size.height / 3 - 50,
                   width: MediaQuery.of(context).size.width / 1),
+              const SizedBox(
+                height: 20,
+              ),
               Form(
-                  child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        hintText: 'Enter email address',
-                        prefixIcon: Icon(Icons.email),
-                        labelStyle: TextStyle(color: Colors.black),
-                        labelText: 'Email'),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          hintText: 'Enter email address',
+                          prefixIcon: const Icon(Icons.email),
+                          labelStyle: const TextStyle(color: Colors.black),
+                          labelText: 'Email'),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          hintText: 'Enter your password',
+                          suffixIcon: const Icon(Iconsax.eye_slash),
+                          prefixIcon: const Icon(Icons.lock),
+                          labelStyle: const TextStyle(color: Colors.black),
+                          labelText: 'Password'),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  child: const Text(
+                    'Login',
                   ),
-                  SizedBox(height: 20,),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        hintText: 'Enter your password',
-                        prefixIcon: Icon(Icons.email),
-                        labelStyle: TextStyle(color: Colors.black),
-                        labelText: 'Password'),
-                  )
-                ],
-              ))
+                  onPressed: () {
+
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[800],
+                      foregroundColor: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                const Text("Don't have an account?"),
+                TextButton(
+                  child: const Text(
+                    'SignUp',
+                  ),
+                  onPressed: () {
+                    Get.to(()=>SignUPSccreen());
+                  },
+                ),
+              ])
             ],
           ),
         ),
