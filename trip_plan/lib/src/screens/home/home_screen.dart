85c6c06e7738fb03c968d836223b4c119434cb20
Nitin Kumar <nk96109936@gmail.com>
2024-06-05 14:07:58 +0530

    detail screen are added

--- trip_plan/lib/src/screens/home/home_screen.dart
+++ trip_plan/lib/src/screens/home/home_screen.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:trip_plan/src/screens/home/destination_datail.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -72,63 +74,66 @@ class HomeScreen extends StatelessWidget {
                                 controller: controller,
                                   itemCount: 20,
                                   itemBuilder: (context,index){
-                                    return Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Container(
-                                              width: 150,
-                                              height: 120,
-                                              clipBehavior: Clip.antiAlias,
-                                              decoration:  BoxDecoration(
-                                                  borderRadius: BorderRadius.circular(20) // Adjust the radius as needed
-                                              ),
-                                              child: Image.asset(
-                                                'assets/animation/trip_imagee.jpeg',
-                                                fit: BoxFit.cover,
-                                              ),
-                                            ),
-                                            const SizedBox(width: 10,),
-                                            Column(
-                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                              children: [
-                                                const Text("Shimla Manali Tour",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                                                const Row(
-                                                  children: [
-                                                    Icon(Iconsax.location,size: 19,color: Colors.grey,),
-                                                    Text(" Manali",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 15,color: Colors.grey),),
-                                                  ],
+                                    return GestureDetector(
+                                      onTap:()=> Get.to(()=>const DestinationDetails()),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Container(
+                                                width: 150,
+                                                height: 120,
+                                                clipBehavior: Clip.antiAlias,
+                                                decoration:  BoxDecoration(
+                                                    borderRadius: BorderRadius.circular(20) // Adjust the radius as needed
                                                 ),
-                                                const SizedBox(height: 5,),
-                                                Row(
-                                                  children: [
-                                                    RatingBar.builder(
-                                                      initialRating: 3,
-                                                      minRating: 1,
-                                                      direction: Axis.horizontal,
-                                                      allowHalfRating: true,
-                                                      itemCount: 5,
-                                                      itemSize: 23,
-                                                      itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
-                                                      itemBuilder: (context, _) => const Icon(
-                                                        Icons.star,
-                                                        color: Colors.amber,
+                                                child: Image.asset(
+                                                  'assets/animation/trip_imagee.jpeg',
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                              const SizedBox(width: 10,),
+                                              Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                children: [
+                                                  const Text("Shimla Manali Tour",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                                                  const Row(
+                                                    children: [
+                                                      Icon(Iconsax.location,size: 19,color: Colors.grey,),
+                                                      Text(" Manali",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 15,color: Colors.grey),),
+                                                    ],
+                                                  ),
+                                                  const SizedBox(height: 5,),
+                                                  Row(
+                                                    children: [
+                                                      RatingBar.builder(
+                                                        initialRating: 3,
+                                                        minRating: 1,
+                                                        direction: Axis.horizontal,
+                                                        allowHalfRating: true,
+                                                        itemCount: 5,
+                                                        itemSize: 23,
+                                                        itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+                                                        itemBuilder: (context, _) => const Icon(
+                                                          Icons.star,
+                                                          color: Colors.amber,
+                                                        ),
+                                                        onRatingUpdate: (rating) {
+                                                          print(rating);
+                                                        },
                                                       ),
-                                                      onRatingUpdate: (rating) {
-                                                        print(rating);
-                                                      },
-                                                    ),
-                                                    const Text(" (29)",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),)
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
-                                          ],
-                                        ),
-                                        const Divider(),
-                                      ],
+                                                      const Text(" (29)",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),)
+                                                    ],
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                          const Divider(),
+                                        ],
+                                      ),
                                     );
                                 }
                               ),
