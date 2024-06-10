3e4e70d7743e01a964d1994d31a005acd9118ba2
MasoomJaat <masoomjaat860@gmail.com>
2024-06-10 15:46:08 +0530

    firebase added

--- trip_plan/lib/main.dart
+++ trip_plan/lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:trip_plan/src/screens/onboarding/onboarding_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
